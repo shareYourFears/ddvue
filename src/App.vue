04c06b6425338d6df419b54d9f6ec205b23b27cb
shareYourFears <fish@stick.net>
2016-05-13 08:17:36 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div is="othercomponent"></div>
+</template>
+
+<script>
+import Vue from 'vue'
+import othercomponent from './OtherComponent.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello frodm vue-loader!',
+      currentView: 'test',
+    }
+  }
+}
+</script>
+
+<style>
+.message {
+  color: blue;
+}
+</style>
---
+++ src/OtherComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="message">{{ msg }}</div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      msg: 'component here'
+    }
+  }
+});
+</script>
+
+<style>
+.message {
+  color: blue;
+}
+</style>
